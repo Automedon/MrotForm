b74a97b284a538a07d9d49ff0ab4282b782046f3
Automedon <42299107+Automedon@users.noreply.github.com>
2020-04-12 23:43:54 +0700

    docs

---
+++ docs/precache-manifest.da4aab384e18272c5f130d0d45a7e5a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbedbac91893cb4e68ddea8312fd93c",
+    "url": "/MrotForm/index.html"
+  },
+  {
+    "revision": "567132fa814dd7475eda",
+    "url": "/MrotForm/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "c6bd6856278693eb5cda",
+    "url": "/MrotForm/static/css/main.a9132544.chunk.css"
+  },
+  {
+    "revision": "567132fa814dd7475eda",
+    "url": "/MrotForm/static/js/2.8a5be6b5.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/MrotForm/static/js/2.8a5be6b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bd6856278693eb5cda",
+    "url": "/MrotForm/static/js/main.d4a16d1d.chunk.js"
+  },
+  {
+    "revision": "4d186a88c2fc3b4e7916",
+    "url": "/MrotForm/static/js/runtime-main.c4674f94.js"
+  }
+]);
